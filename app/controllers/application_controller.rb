7d1ced8de4bd1b434fa390d4778ddbdc4ffa3f1d
kaiomagalhaes <me@kaiomagalhaes.com>
2021-06-15 14:39:07 -0300

    add convox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def index
+    render json: { status: 200 }
+  end
+
   def message
     params[:message]
   end
