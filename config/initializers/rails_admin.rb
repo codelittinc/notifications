af378f84501ff4f870bd7b5c8f6ac8e944606280
Vinicius <vini89go@gmail.com>
2021-06-24 11:18:03 -0300

    Add authentication to rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,6 +4,16 @@ require 'nested_form/engine'
 require 'nested_form/builder_mixin'
 
 RailsAdmin.config do |config|
+  if Rails.env.production?
+    config.authorize_with do
+      authenticate_or_request_with_http_basic('Login required') do
+        |username, password|
+        username == ENV['ADMIN_USER'] && password == ENV['ADMIN_PASS']
+      end
+    end
+  end
+
+  config.main_app_name { %w[Notifications Admin] }
   ### Popular gems integration
 
   ## == Devise ==
