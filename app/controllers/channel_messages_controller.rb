51246f427c87a450dda5257b1a2162f233ddfe4f
kaiomagalhaes <me@kaiomagalhaes.com>
2021-07-31 08:07:21 -0300

    refacor message creator

--- app/controllers/channel_messages_controller.rb
+++ app/controllers/channel_messages_controller.rb
@@ -4,16 +4,16 @@ class ChannelMessagesController < ApplicationController
   before_action :authenticate
 
   def create
-    MessageCreator.new(provider_credential, 'create', 'channel', message,
-                       formatted_channel).create!
+    MessageCreator.call(provider_credential, 'create', 'channel', message,
+                        formatted_channel)
 
     render json: client.create_channel_message!(formatted_channel, message,
                                                 timestamp, true)
   end
 
   def update
-    MessageCreator.new(provider_credential, 'update', 'channel', message,
-                       formatted_channel).create!
+    MessageCreator.call(provider_credential, 'update', 'channel', message,
+                        formatted_channel)
 
     render json: client.update_message!(formatted_channel, message, timestamp,
                                         true)
