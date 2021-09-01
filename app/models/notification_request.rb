75258864793630ecc493a24a6a3f07086550a189
raphaelsattler <raphaelsattler@hotmail.com>
2021-09-01 16:43:08 -0300

    Update Message and Notification Request

--- app/models/notification_request.rb
+++ app/models/notification_request.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class NotificationRequest < ApplicationRecord
+  has_one :message, dependent: :destroy
+
   belongs_to :provider_credential
 end
--- db/migrate/20210801144110_add_json_to_notification_request.rb
+++ db/migrate/20210801144110_add_json_to_notification_request.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddJsonToNotificationRequest < ActiveRecord::Migration[6.1]
   def change
     add_column :notification_requests, :json, :string
