0fc400d6deb573dd9713df5f0e7d4ca4dad02ca8
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-08-11 23:53:57 +0200

    add annotate gem

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: messages
+#
+#  id                      :bigint           not null, primary key
+#  text                    :string
+#  target_type             :string
+#  action                  :string
+#  target                  :string
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  provider_credential_id  :bigint
+#  target_identifier       :string
+#  notification_request_id :bigint
+#
 class Message < ApplicationRecord
   belongs_to :provider_credential
   belongs_to :notification_request, optional: true
