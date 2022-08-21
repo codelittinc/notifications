5b5c6850faecb1fd409d3332264e0f9a2557d66d
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-08-21 11:55:20 +0200

    add pull prod backup script

---
+++ bin/pull_prod_backup.sh
@@ -0,0 +1,10 @@
+rm latest.dump
+docker-compose up -d
+docker stop notifications-api
+docker stop notifications-sidekiq
+heroku pg:backups:capture --app prod-notifications
+heroku pg:backups:download --app prod-notifications
+docker exec -it notifications-db psql -U postgres -c 'DROP DATABASE IF EXISTS notifications_development'
+docker exec -it notifications-db psql -U postgres -c "CREATE DATABASE notifications_development"
+docker exec -it notifications-db pg_restore --no-owner  -U postgres -d notifications_development -1 ./app/latest.dump
+sh bin/dev
