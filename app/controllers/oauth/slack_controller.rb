92e9145bf4aba4c94f19ab49f643648eb5a64cfd
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-10-09 08:58:29 -0300

    redirect to url after authentication

--- app/controllers/oauth/slack_controller.rb
+++ app/controllers/oauth/slack_controller.rb
@@ -1,23 +1,40 @@
 # frozen_string_literal: true
 
+require 'uri'
+require 'net/http'
+
 module Oauth
   class SlackController < ApplicationController
     def create
-      code = params[:code]
-      data = Clients::Slack::Oauth.new(code).authenticate!
+      data = authenticate!
+
       access_key = data.with_indifferent_access[:access_token]
       team_id = data.with_indifferent_access[:team][:id]
       team_name = data.with_indifferent_access[:team][:name]
 
       authorization_bearer = save_credentials!(team_id, team_name, access_key)
 
-      render json: {
-        status: 200,
-        application_key: authorization_bearer
+      query = {
+        application_key: authorization_bearer,
+        team_name:,
+        team_id:
       }
+
+      encoded_data = URI.encode_www_form(query)
+
+      redirect_to URI.parse("#{roadrunner_url}?#{encoded_data}").to_s
     end
 
     private
+
+    def authenticate!
+      code = params[:code]
+      Clients::Slack::Oauth.new(code).authenticate!
+    end
+
+    def roadrunner_url
+      @roadrunner_url ||= ENV.fetch('ROADRUNNER_URL', nil)
+    end
 
     def save_credentials!(team_id, team_name, access_key)
       credentials = ProviderCredential.find_or_initialize_by(
