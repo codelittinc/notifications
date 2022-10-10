536a0b70d19f6b10f34c0ea6b808064d6837217b
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-10-10 09:46:39 -0300

    create organization on roadrunner

--- app/controllers/oauth/slack_controller.rb
+++ app/controllers/oauth/slack_controller.rb
@@ -8,43 +8,47 @@ module Oauth
     def create
       data = authenticate!
 
-      access_key = data.with_indifferent_access[:access_token]
-      team_id = data.with_indifferent_access[:team][:id]
-      team_name = data.with_indifferent_access[:team][:name]
+      credentials = save_credentials!(data)
 
-      authorization_bearer = save_credentials!(team_id, team_name, access_key)
+      response = Request.post(organizations_url, '', organizations_body(credentials))
+      organizations_id = JSON.parse(response.body)['id']
 
-      query = {
-        application_key: authorization_bearer,
-        team_name:,
-        team_id:
-      }
+      organizations_path = URI.parse("#{roadrunner_url}/organizations/#{organizations_id}").to_s
 
-      encoded_data = URI.encode_www_form(query)
-
-      redirect_to URI.parse("#{roadrunner_url}?#{encoded_data}").to_s
+      redirect_to organizations_path
     end
 
     private
+
+    def organizations_body(credentials)
+      {
+        notifications_key: credentials.application_key,
+        name: credentials.team_name,
+        notifications_id: credentials.id
+      }
+    end
 
     def authenticate!
       code = params[:code]
       Clients::Slack::Oauth.new(code).authenticate!
     end
 
+    def organizations_url
+      "#{roadrunner_url}/organizations"
+    end
+
     def roadrunner_url
       @roadrunner_url ||= ENV.fetch('ROADRUNNER_URL', nil)
     end
 
-    def save_credentials!(team_id, team_name, access_key)
-      credentials = ProviderCredential.find_or_initialize_by(
-        team_id:
-      )
-      credentials.team_name = team_name
-      credentials.access_key = access_key
-      credentials.application_key = Base64.strict_encode64(access_key)
+    def save_credentials!(data)
+      team_id = data.with_indifferent_access[:team][:id]
+      credentials = ProviderCredential.find_or_initialize_by(team_id:)
+      credentials.team_name = data.with_indifferent_access[:team][:name]
+      credentials.access_key = data.with_indifferent_access[:access_token]
+      credentials.application_key = Base64.strict_encode64(credentials.access_key)
       credentials.save!
-      credentials.application_key
+      credentials
     end
   end
 end
