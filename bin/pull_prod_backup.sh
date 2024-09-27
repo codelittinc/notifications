6e268eb0613b78c04bdd25bbbe9aa9924699e569
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-09-27 14:25:55 -0300

    fix pull prod

--- bin/pull_prod_backup.sh
+++ bin/pull_prod_backup.sh
@@ -6,5 +6,6 @@ heroku pg:backups:capture --app prod-notifications
 heroku pg:backups:download --app prod-notifications
 docker exec -it notifications-db psql -U postgres -c 'DROP DATABASE IF EXISTS notifications_development'
 docker exec -it notifications-db psql -U postgres -c "CREATE DATABASE notifications_development"
+docker exec -it notifications-db psql -U postgres notifications_development -c "CREATE SCHEMA IF NOT EXISTS heroku_ext AUTHORIZATION postgres"
 docker exec -it notifications-db pg_restore --no-owner  -U postgres -d notifications_development -1 ./app/latest.dump
 sh bin/dev
